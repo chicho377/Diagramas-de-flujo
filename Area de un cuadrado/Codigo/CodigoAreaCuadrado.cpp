0f993cc73edd4641d876d8bcb6e0dbf921e1a596
chicho377 <morafabricio86@gmail.com>
2024-05-29 23:50:26 -0600

    Agregando codigo area de un cuadrado

---
+++ Area de un cuadrado/Codigo/CodigoAreaCuadrado.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+ #include <stdio.h>
+ #include <conio.h>
+ int lado,area;  
+int main()
+ {
+      system("cls"); 
+ printf("Cual es el valor de un lado:");
+      scanf("%i",&lado); 
+      area=lado*lado;
+      printf("El área es:%i\n",area); 
+ getch();
+      return 0;
+ } 
